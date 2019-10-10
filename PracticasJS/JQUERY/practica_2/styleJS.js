c911d5b9500679f16c85a91a048c27d805fa00a9
ronaldo0702 <aldo07022614@hotmail.com>
2019-10-10 09:01:21 -0500

    Practica 2 JS

---
+++ PracticasJS/JQUERY/practica_2/styleJS.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.cuadro').css({border: '3px solid black'})
+    $('.btnes').click(function(){
+        $('.cuadro:first').css({background:'blue'})
+        $('.cuadro:last').css({background:'blue'})
+
+
+    })
+
+})
